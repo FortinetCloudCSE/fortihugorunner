ba93b42e9750a8202b590731390812d13879c5d1
Robert Reris <rreris@fortinet.com>
2025-03-04 13:45:16 -0800

    changing default windows binaries to include 386

--- build_binaries.sh
+++ build_binaries.sh
@@ -4,4 +4,4 @@ GOOS=linux GOARCH=arm64 go build -o binaries/docker-run-go-linux-arm64 . && echo
 GOOS=darwin GOARCH=amd64 go build -o binaries/docker-run-go-mac-amd64 . && echo "Mac AMD built"
 GOOS=darwin GOARCH=arm64 go build -o binaries/docker-run-go-mac-arm64 . && echo "Mac ARM built"
 GOOS=windows GOARCH=amd64 go build -o binaries/docker-run-go-windows-amd64.exe . && echo "Windows AMD built"
-GOOS=windows GOARCH=arm64 go build -o binaries/docker-run-go-windows-arm64.exe . && echo "Windows ARM built"
+GOOS=windows GOARCH=386 go build -o binaries/docker-run-go-windows-386.exe . && echo "Windows 386 built"
