606d260ba3e67058155db228be6a7dbc9c7852e6
Robert Reris <rreris@fortinet.com>
2025-03-07 11:42:09 -0800

    v0.1.0 release

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## [v1.0.0] - 2025-03-07
+### Added
+- Initial release with core features:
+  - build administrative and development Docker images
+  - launch a Hugo server container for local workshop development
+- Support for Windows, MacOs, and Linux architectures
--- build_binaries.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-GOOS=linux GOARCH=amd64 go build -o binaries/docker-run-go-linux-amd64 . && echo "Linux AMD built"
-GOOS=linux GOARCH=arm64 go build -o binaries/docker-run-go-linux-arm64 . && echo "Linux ARM built"
-GOOS=darwin GOARCH=amd64 go build -o binaries/docker-run-go-mac-amd64 . && echo "Mac AMD built"
-GOOS=darwin GOARCH=arm64 go build -o binaries/docker-run-go-mac-arm64 . && echo "Mac ARM built"
-GOOS=windows GOARCH=amd64 go build -o binaries/docker-run-go-windows-amd64.exe . && echo "Windows AMD built"
-GOOS=windows GOARCH=386 go build -o binaries/docker-run-go-windows-386.exe . && echo "Windows 386 built"
